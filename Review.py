51b348888544e1c5127cc52acdad5c57782645fc
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-27 22:43:42 +0300

    Added a check that ensures the rating given is either a float or a whole number else it raises a value error and added the customer and restaurant methods.

--- Review.py
+++ Review.py
@@ -4,14 +4,23 @@ class Review:
 
     # Initialize a review object with a customer, restaurant, and rating
     def __init__(self, customer, restaurant, rating):
+        if type(rating) != int or type(rating) != float:
+            raise ValueError("Rating should be a number")
+        
         self._customer = customer
         self._restaurant = restaurant
         self._rating = rating
         Review.all_reviews.append(self)  # Add the review instance to the all_reviews list
 
-    # Return the rating of the review
+    # Return the rating of the review, the customer and the restaurant
     def rating(self):
         return self._rating
+    
+    def customer(self):
+        return self._customer
+    
+    def restaurant(self):
+        return self._restaurant
     
     # A class method to return all the review instances
     @classmethod
